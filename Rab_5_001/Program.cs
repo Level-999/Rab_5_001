fb239745cb9c4a4affecf9dd04debc0bab0ae899
Level-999 <dalsep@mail.ru>
2021-12-05 21:58:07 +1000

    Добавьте файлы проекта.

---
+++ Rab_5_001/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Rab_5_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сформировать одномерный массив из 7 элементов. Заполнить массив числами,
+            //вводимыми с клавиатуры, определить среднее арифметическое элементов.
+
+
+            const int n = 7;
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write($"Введите элемент массива {i}\t");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив");
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое элементов массива");
+            float  sredn = 0;
+            for (int i = 0; i < array.Length; i++)
+                sredn += array[i];
+            float sa =sredn / array.Length;
+          
+            Console.WriteLine();
+            Console.Write("{0:f3} ",sa) ;
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Rab_5_002/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Rab_5_002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запросить у пользователя целочисленное значение N.
+            // Сформировать двумерный массив размера NxN следующего вида:
+            //  1 0 1 0 1                        
+            //  0 1 0 1 0
+            //  1 0 1 0 1
+            //  0 1 0 1 0
+            //  1 0 1 0 1
+
+            const int n = 5;
+            const int m = 5;
+            int[,] a = new int [n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = 1 - (i + j) % 2;
+                    Console.Write(a[i, j] + " ");
+                }
+
+                Console.WriteLine();
+                
+            }
+
+        }
+    }
+}
